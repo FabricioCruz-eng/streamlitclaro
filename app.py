a076f04bf9cfe74850c48451a8eaaa90b947aa83
FabricioCruz-eng <149114588+FabricioCruz-eng@users.noreply.github.com>
2025-06-10 00:39:42 -0300

    Update app.py
    dddddddd

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ Ferramenta desenvolvida e mantida por fabricio.cruz@claro.com.br
 ### Dados Utilizados
 Os arquivos utilizados na análise estão disponíveis em:
 1. 🗃️ Repositório de Dados do Portal Roma 
-2. 🔓 Trechos Cedidos """)
+2. 🔓 Trechos Cedidos""")
 
 # Adicionando rodapé com informações de direitos autorais
 st.markdown("---")
