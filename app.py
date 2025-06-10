f9ac5fbc494802a551715520d4862409c14c8741
FabricioCruz-eng <149114588+FabricioCruz-eng@users.noreply.github.com>
2025-06-10 10:20:53 -0300

    Update app.py
    mudança de cor

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.set_page_config(page_title="Pesquisa de Rotas", layout="wide")
 st.markdown("""
     <h1 style="font-size: 24px; color: #DA291C;">🛣️ Trecho Claro Cedente</h1>
-""", unsafe_allow_html=True)
+""",unsafe_allow_html=True)
 
 # Upload do arquivo
 dados = None
