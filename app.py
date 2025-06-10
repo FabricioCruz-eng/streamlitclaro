fbc8a4ea1b635b176c388f3c190d3f0e738772b9
FabricioCruz-eng <149114588+FabricioCruz-eng@users.noreply.github.com>
2025-06-10 10:16:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.set_page_config(page_title="Pesquisa de Rotas", layout="wide")
 st.markdown("""
-    <h1 style="font-size: 24px; color: #FFAF02;">🛣️ Trecho Claro Cedente</h1>
+    <h1 style="font-size: 24px; color: #DA291C;">🛣️ Trecho Claro Cedente</h1>
 """, unsafe_allow_html=True)
 
 # Upload do arquivo
